b4f86c9d7e60f5c1caae37d5c24d421a43a00c22
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2023-08-27 12:05:06 -0600

    Saltos por tecla PRODUCTO

--- web/archivos/script.js
+++ web/archivos/script.js
@@ -7,7 +7,20 @@
 function habilita(idElemento, idElemento2) {
     document.getElementById(idElemento).style.display = 'block'; //Mostrar
     document.getElementById(idElemento2).style.display = 'none'; //Ocultar
-    
+
     //Se afecta la propieda display para que este muestre u oculte algo. 
-    
+
+}
+
+function foco(idElemento) {
+    document.getElementById(idElemento).focus();
+}
+
+function focoBoton(botonGuardar, botonModificar) {
+    if (document.getElementById(botonGuardar).style.display === "block") {
+        document.getElementById(botonGuardar).focus();
+    }
+    if (document.getElementById(botonModificar).style.display === "block") {
+        document.getElementById(botonModificar).focus();
+    }
 }
